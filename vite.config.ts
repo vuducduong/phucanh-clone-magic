8cc5e6130fb136b52ba967581afde24a94a9f9c5
duongvd <vd-duong@jit.vn>
2025-11-13 20:35:44 +0700

    update code

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// Đặt tên repo của bạn ở đây
+const repoName = "lovable-web";
+
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`, // <--- thêm dòng này
   server: {
     host: "::",
     port: 8080,
